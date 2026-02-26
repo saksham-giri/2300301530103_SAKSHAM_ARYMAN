b1ecb8776a7e1b7b0ab2a50beb36ea27a917aa28
saksham-giri <sakshamgiri027@gmail.com>
2026-02-27 01:29:20 +0530

    assignment 26-02-2026

---
+++ 26-02-2026/Assignment/q1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int findFloor(vector<int>& arr, int x) {
+        // code here
+        int left=0,right=arr.size()-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(arr[mid]>x) right=mid-1;
+            else left=mid+1;
+        }
+        return right;
+    }
+};
---
+++ 27-02-2026/Assignment/Q2.cpp
@@ -0,0 +1,8 @@
+1 : Make a repo on git hub (ROLLNO_YOURNAME_ARYMAN)
+2 : Make A folder on your desktop and open it on vs code
+3 : git init
+4 : git add .
+4 : git commit -m "Initial commit"
+5 : git branch -M main 
+6 : git remote add origin url
+7 : git push -u origin main
